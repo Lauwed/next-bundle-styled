6752808167e33ecea292a44b3fc307da5dc31426
Lauwed <ldurieux97@gmail.com>
2021-06-20 13:21:22 +0200

    Adding basic style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import globalStyles from "../assets/styles/global";
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {globalStyles}
+      <Component {...pageProps} />
+    </>
+  )
 }
 export default MyApp
